c19accfa60248097411d8d0c5d477d1365d138d8
iznomag <iznomag@gmail.com>
2020-11-01 20:21:11 +0100

    Add extra exercise 4

---
+++ src/__tests__/exercise/04.extra-3.js
@@ -0,0 +1,31 @@
+// form testing
+// http://localhost:3000/login
+
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Login from '../../components/login'
+import faker from 'faker'
+
+function buildLoginForm(data) {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...data,
+  }
+}
+
+test('submitting the form calls onSubmit with username and password', () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  const {username, password} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+})
---
+++ src/__tests__/exercise/04.extra-4.js
@@ -0,0 +1,30 @@
+// form testing
+// http://localhost:3000/login
+
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const userBuilder = build('User', {
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
+test('submitting the form calls onSubmit with username and password', () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  const {username, password} = userBuilder()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+})
